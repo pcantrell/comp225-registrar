ea470178cada3e244f53c9119db4ab9628cbad9f
Shurlow <Scottyhurlow@gmail.com>
2015-03-11 20:40:01 -0500

    I think everything is working correctly

--- src/edu/macalester/registrar/Course.java
+++ src/edu/macalester/registrar/Course.java
@@ -3,11 +3,16 @@ package edu.macalester.registrar;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 
 public class Course {
     private String catalogNumber, title;
     private Set<Student> students = new HashSet<Student>();
+    private Queue<Student> waitList = new LinkedList<Student>();
+    private int courseLimit;
 
     public String getCatalogNumber() {
         return catalogNumber;
@@ -25,11 +30,37 @@ public class Course {
         this.title = title;
     }
 
+    public void setCourseLimit(int i) {
+        this.courseLimit = i;
+    }
+
+    public void addToWaitList(Student s) {
+        waitList.add(s);
+    }
+
+    public void resolveWaitlist() {
+        if (!this.isFull()) {
+            try {
+                waitList.remove().enrollIn(this);
+            } catch (NoSuchElementException e) {
+                System.out.println("No students in wait list to add.");
+            }
+        }
+    }
+
+    public boolean isFull() {
+        return students.size() == courseLimit;
+    }
+
     public Set<Student> getStudents() {
         return Collections.unmodifiableSet(students);
     }
 
-    void enroll(Student student) {
+    public void dropStudent(Student s) {
+        students.remove(s);
+    }
+
+    public void enroll(Student student) {
         students.add(student);
     }
 }
--- src/edu/macalester/registrar/Student.java
+++ src/edu/macalester/registrar/Student.java
@@ -3,6 +3,7 @@ package edu.macalester.registrar;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+// import java.lang.Exception;
 
 
 public class Student {
@@ -21,13 +22,27 @@ public class Student {
         return Collections.unmodifiableSet(courses);
     }
 
+    public void dropCourse(Course course) {
+        if (course.getStudents().contains(this)) {
+            courses.remove(course);
+            course.dropStudent(this);
+            course.resolveWaitlist();    
+        }
+    }
+
     /**
      * Add this student to the given course's roster.
      * Has no effect if the student is already registered.
      * Equivalent to course.enroll(student).
      */
     public void enrollIn(Course course) {
-        courses.add(course);
-        course.enroll(this);
+        if (!course.isFull()) {
+            courses.add(course);
+            course.enroll(this);
+        } else if (!course.getStudents().contains(this)) {
+            course.addToWaitList(this);
+            System.out.println("Student added to wait list for " + course.getCatalogNumber());
+        }
+        
     }
 }
