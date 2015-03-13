f034467091a69ead4056691d925135e45a7cb6cb
anguyen12 <anguyen@macaleser.edu>
2015-03-12 22:54:03 -0500

    made changes to enrollment

--- src/edu/macalester/registrar/Course.java
+++ src/edu/macalester/registrar/Course.java
@@ -1,5 +1,6 @@
 package edu.macalester.registrar;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,6 +9,8 @@ import java.util.Set;
 public class Course {
     private String catalogNumber, title;
     private Set<Student> students = new HashSet<Student>();
+    private ArrayList<Student> waitList = new ArrayList<Student>();
+    private int enrollmentLimit = 1;
 
     public String getCatalogNumber() {
         return catalogNumber;
@@ -21,6 +24,10 @@ public class Course {
         return title;
     }
 
+    public int getEnrollmentLimit() {
+        return enrollmentLimit;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -30,6 +37,10 @@ public class Course {
     }
 
     void enroll(Student student) {
-        students.add(student);
+        if (students.size() < getEnrollmentLimit())
+            students.add(student);
+        else
+        if (!students.contains(student))
+            waitList.add(student);
     }
 }
