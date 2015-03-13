dbed8f31b5ed0bc982377eec179b8afd0c5295e2
Derek Schindelman <dschindelman@gmail.com>
2015-03-13 10:11:21 -0500

    Completed the assignment

--- src/edu/macalester/registrar/Course.java
+++ src/edu/macalester/registrar/Course.java
@@ -1,14 +1,18 @@
 package edu.macalester.registrar;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
 public class Course {
     private String catalogNumber, title;
     private Set<Student> students = new HashSet<Student>();
-
+    private List<Student> waitList = new ArrayList<Student>();
+    private int enrollmentLimit = NO_ENROLLMENT_LIMIT;
+    
     public String getCatalogNumber() {
         return catalogNumber;
     }
@@ -28,8 +32,64 @@ public class Course {
     public Set<Student> getStudents() {
         return Collections.unmodifiableSet(students);
     }
-
-    void enroll(Student student) {
-        students.add(student);
+    
+    public List<Student> getWaitList() {
+        return Collections.unmodifiableList(waitList);
     }
+    
+    public int getEnrollmentLimit() {
+        return enrollmentLimit;
+    }
+    
+    public void setEnrollmentLimit(int enrollmentLimit) {
+        if (enrollmentLimit == NO_ENROLLMENT_LIMIT) {
+            this.enrollmentLimit = enrollmentLimit;
+            while (!waitList.isEmpty()) {
+                Student student = waitList.remove(0);
+                student.enrollIn(this);
+            }
+        } else {
+            if (enrollmentLimit < this.enrollmentLimit) {
+                throw new IllegalArgumentException("The enrollment limit cannot be decreased.");
+            }
+            
+            if (enrollmentLimit < students.size()) {
+                throw new IllegalArgumentException("The enrollment limit cannot be set lower than the current number of students.");
+            }
+
+            int difference = enrollmentLimit - this.enrollmentLimit;
+            this.enrollmentLimit = enrollmentLimit;
+            
+            if (!waitList.isEmpty()) {
+                int admitCount = Math.min(difference, waitList.size());
+                for (int i = 0; i < admitCount; i++) {
+                    Student student = waitList.remove(0);
+                    student.enrollIn(this);
+                }
+            }
+        }
+    }
+
+    boolean enroll(Student student) {
+        if (enrollmentLimit != NO_ENROLLMENT_LIMIT) {
+            if (enrollmentLimit <= students.size()) {
+                waitList.add(student);
+                return false;
+            }
+        }
+
+        students.add(student);
+        return true;
+    }
+    
+    void drop(Student student) {
+        if (students.remove(student)) {
+            if (!waitList.isEmpty()) {
+                Student fromWaitList = waitList.remove(0);
+                fromWaitList.enrollIn(this);
+            }
+        }
+    }
+    
+    public static final int NO_ENROLLMENT_LIMIT = -1;
 }
--- src/edu/macalester/registrar/RegistrarTest.java
+++ src/edu/macalester/registrar/RegistrarTest.java
@@ -5,66 +5,175 @@ package edu.macalester.registrar;
  */
 public class RegistrarTest {
     public static void main(String[] args) {
-        // Example students
-
-        Student sally = new Student();
-        sally.setName("Sally");
-
-        Student fred = new Student();
-        fred.setName("Fred");
-
-        // Example courses
-
-        Course c1 = new Course();
-        c1.setCatalogNumber("COMP 225");
-        c1.setTitle("Software Fun Fun");
-
-        Course c2 = new Course();
-        c2.setCatalogNumber("MATH 6");
-        c2.setTitle("All About the Number Six");
-
-        System.out.println("------ Enrolling Sally in two courses ------");
-
-        sally.enrollIn(c1);
-        sally.enrollIn(c2);
-
-        printSchedule(sally);
-        printSchedule(fred);
-
-        printEnrollment(c1);
-        printEnrollment(c2);
-
-        System.out.println("------ Enrolling Fred in one course ------");
-
-        fred.enrollIn(c2);
-
-        printSchedule(fred);
-        printEnrollment(c1);
-        printEnrollment(c2);
-
-        System.out.println("------ Re-enrolling Sally has no effect ------");
-
-        sally.enrollIn(c1);
-
-        printSchedule(sally);
-        printEnrollment(c1);
+        enrollTest();
+        enrollmentLimitTest();
+        dropTest();
     }
 
-    private static void printSchedule(Student student) {
-        System.out.println("Student name: " + student.getName());
-        System.out.println("Courses (" + student.getCourses().size() + ")");
-        for(Course course : student.getCourses())
-            System.out.println("    "
-                + course.getCatalogNumber() + ": "
-                + course.getTitle());
-        System.out.println();
+    public static void enrollTest() {
+        Course geography = createCourse("GEOG 314", "Geography and Lasers");
+        Course math = createCourse("MATH 412", "Calculus with Apples");
+
+        Student sally = createStudent("Sally");
+        Student fred = createStudent("Fred");
+
+        assert math.getStudents().size() == 0;
+        assert math.getWaitList().size() == 0;
+        assert sally.getCourses().size() == 0;
+
+        sally.enrollIn(math);
+        sally.enrollIn(geography);
+        fred.enrollIn(math);
+
+        // Math should have both Sally and Fred enrolled
+        assert math.getStudents().contains(sally);
+        assert math.getStudents().contains(fred);
+
+        // Geography should have Sally enrolled
+        assert geography.getStudents().contains(sally);
+
+        // Sally should be enrolled in both Geography and Math
+        assert sally.getCourses().contains(math);
+        assert sally.getCourses().contains(geography);
+
+        // Fred should be enrolled in Math
+        assert fred.getCourses().contains(math);
+
+        // Fred should not be enrolled in Geography
+        assert !fred.getCourses().contains(geography);
+
+        // Geography should not have Fred enrolled
+        assert !geography.getStudents().contains(fred);
     }
 
-    private static void printEnrollment(Course course) {
-        System.out.println(course.getCatalogNumber() + ": " + course.getTitle());
-        System.out.println("Students enrolled (" + course.getStudents().size() + ")");
-        for(Student student : course.getStudents())
-            System.out.println("    " + student.getName());
-        System.out.println();
+    public static void enrollmentLimitTest() {
+        Course geography = createCourse("GEOG 314", "Geography and Lasers");
+        Course math = createCourse("MATH 412", "Calculus with Apples");
+
+        Student sally = createStudent("Sally");
+        Student greg = createStudent("Greg");
+        Student fred = createStudent("Fred");
+        Student emily = createStudent("Emily");
+
+        // There should be no enrollment limit by default
+        assert math.getEnrollmentLimit() == Course.NO_ENROLLMENT_LIMIT;
+        assert geography.getEnrollmentLimit() == Course.NO_ENROLLMENT_LIMIT;
+
+        // It should be possible to set an enrollment limit
+        math.setEnrollmentLimit(2);
+        assert math.getEnrollmentLimit() == 2;
+
+        // Adding students up to the enrollment limit should work
+        sally.enrollIn(math);
+        greg.enrollIn(math);
+        assert sally.getCourses().contains(math);
+        assert greg.getCourses().contains(math);
+        assert math.getStudents().contains(sally);
+        assert math.getStudents().contains(greg);
+
+        // Extra students added should go on the wait list
+        boolean fredSucceeded = fred.enrollIn(math);
+        boolean emilySucceeded = emily.enrollIn(math);
+        assert !fredSucceeded;
+        assert !emilySucceeded;
+        assert !fred.getCourses().contains(math);
+        assert !emily.getCourses().contains(math);
+        assert !math.getStudents().contains(fred);
+        assert !math.getStudents().contains(emily);
+        assert math.getWaitList().get(0) == fred;
+        assert math.getWaitList().get(1) == emily;
+
+        // Increasing the enrollment limit should add students from the wait list
+        math.setEnrollmentLimit(3);
+        assert fred.getCourses().contains(math);
+        assert math.getStudents().contains(fred);
+        assert !emily.getCourses().contains(math);
+        assert !math.getStudents().contains(emily);
+        assert math.getWaitList().get(0) == emily;
+
+        // Removing the enrollment limit should admit all students from the wait list
+        math.setEnrollmentLimit(Course.NO_ENROLLMENT_LIMIT);
+        assert emily.getCourses().contains(math);
+        assert math.getStudents().contains(emily);
+        assert math.getWaitList().isEmpty();
+
+        // It should not be possible to reduce the enrollment limit once set,
+        // unless it has subsequently been removed
+        geography.setEnrollmentLimit(2);
+        boolean reducedEnrollmentLimit = false;
+        try {
+            geography.setEnrollmentLimit(1);
+            reducedEnrollmentLimit = true;
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        assert !reducedEnrollmentLimit;
+
+        // It should not be possible to set the enrollment limit to a
+        // value smaller than the number of students in the class
+        geography.setEnrollmentLimit(Course.NO_ENROLLMENT_LIMIT);
+        fred.enrollIn(geography);
+        emily.enrollIn(geography);
+        assert geography.getStudents().size() == 2;
+
+        boolean setEnrollmentLimitTooSmall = false;
+        try {
+            geography.setEnrollmentLimit(1);
+            setEnrollmentLimitTooSmall = true;
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        assert !setEnrollmentLimitTooSmall;
+    }
+
+    public static void dropTest() {
+        Course math = createCourse("MATH 412", "Calculus with Apples");
+
+        Student sally = createStudent("Sally");
+        Student greg = createStudent("Greg");
+        Student fred = createStudent("Fred");
+
+        math.setEnrollmentLimit(2);
+
+        sally.enrollIn(math);
+        greg.enrollIn(math);
+        fred.enrollIn(math);
+
+        // Sally and Greg should both be enrolled in Math
+        assert math.getStudents().contains(sally);
+        assert math.getStudents().contains(greg);
+        assert sally.getCourses().contains(math);
+        assert greg.getCourses().contains(math);
+
+        // Fred should not be enrolled in Math
+        assert !math.getStudents().contains(fred);
+        assert !fred.getCourses().contains(math);
+
+        greg.drop(math);
+
+        // After Greg drops Math, Sally and Fred should be in Math
+        assert math.getStudents().contains(sally);
+        assert math.getStudents().contains(fred);
+        assert sally.getCourses().contains(math);
+        assert fred.getCourses().contains(math);
+
+        // Greg should no longer be in Math
+        assert !math.getStudents().contains(greg);
+        assert !greg.getCourses().contains(math);
+    }
+
+    private static Course createCourse(String catalogNumber, String title) {
+        Course course = new Course();
+        course.setCatalogNumber(catalogNumber);
+        course.setTitle(title);
+        return course;
+    }
+
+    private static Student createStudent(String name) {
+        Student student = new Student();
+        student.setName(name);
+        return student;
     }
 }
